abc9904d0dbfd2d71afe7f84c5d3617b32640154
Naveen Akalanka <naveenakalanka70@gmail.com>
2025-10-02 00:38:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center 
+                    bg-gradient-to-r from-purple-500 via-pink-500 to-red-500">
+      <h1 className="text-5xl font-extrabold text-white drop-shadow-lg">
+        React + Tailwind Template 🚀
+      </h1>
+    </div>
+  )
+}
